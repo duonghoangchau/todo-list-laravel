81ec6f4f27b0c99743124ae462a5fbdf3210936d
HOANGCHAU <duonghoangchau345@gmail.com>
2026-02-25 10:57:24 +0700

    add url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,24 +5,22 @@ namespace App\Providers;
 use App\Models\Todo;
 use App\Policies\TodoPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL; 
 use Illuminate\Support\ServiceProvider;
-
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Gate::policy(Todo::class, TodoPolicy::class);
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
